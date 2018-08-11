ff238934f403640eafcb6f2a62e89720c5bf0c63
guomzh <1361097727@qq.com>
2018-08-11 14:03:20 +0800

    修改打包方式未war

--- src/main/java/com/zgm/myscrum/MyScrumApplication.java
+++ src/main/java/com/zgm/myscrum/MyScrumApplication.java
@@ -3,12 +3,17 @@ package com.zgm.myscrum;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.zgm.myscrum.dao")
-public class MyScrumApplication {
-
+public class MyScrumApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MyScrumApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+           return builder.sources(MyScrumApplication.class);
 	}
 }
